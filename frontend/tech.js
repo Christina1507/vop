c538d0e1ae73d3bf6e55de6063f4aed313f4d548
Christina1507 <christinasharon157@gmail.com>
2026-04-01 22:03:08 +0530

    Update tech.js

--- frontend/tech.js
+++ frontend/tech.js
@@ -10,7 +10,7 @@ async function submitProfile() {
     category: "tech"
   };
 
-  await fetch("http://localhost:5000/api/volunteer", {
+  await fetch("http://13.203.224.107:5000/api/volunteer", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
